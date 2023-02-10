7fa3b5ebde0defab2c33410cead1235cbc05762b
Alex Kuznetsov <alexxq@list.ru>
2023-02-11 01:30:10 +0300

    Beta version for_each_no_skip_first_iteration

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 public class Main {
     static int max1 = Integer.MIN_VALUE;
-    static int max2 = Integer.MIN_VALUE;
+    static int max2 = max1;
+
     public static void main(String[] args) {
-        int[] array = new int[]{-1000, 2, 2, 21, 0, 2, 2};
-        boolean firstIteration = true;
-        for (int i : array){
-            if (firstIteration) firstIteration = false;
-            else
-                if (max1 < i) {
-                    max2 = max1;
-                    max1 =i;
-                }
-                else if (max2 < i) max2 = i;
-            }
+        int[] array = new int[]{1000, 2, 2, 21, 0, 2, 2};
+        for (int i : array) {
+            if (max1 < i) {
+                max2 = max1;
+                max1 = i;
+            } else if (max2 < i) max2 = i;
+        }
         System.out.printf("Самый большой элемент массива: %d\n", max1);
         System.out.printf("Второй по величине элемент массива: %d", max2);
     }

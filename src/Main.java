0c36b09d588a54f089294b4bbefb80957288e33b
Alex Kuznetsov <alexxq@list.ru>
2023-02-11 01:46:24 +0300

    for_each_no_skip_first_iteration second edition

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,16 @@ public class Main {
     static int max2 = max1;
 
     public static void main(String[] args) {
+//        int []array = new int[] {1,2};
         int[] array = new int[]{1000, 2, 2, 21, 0, 2, 2};
+        if (array.length == 0) {
+            System.out.println("Пустой массив");
+            return;
+        }
+        if (array.length == 1) {
+            System.out.printf("В массиве один элемент:   %d", array[0]);
+            return;
+        }
         for (int i : array) {
             if (max1 < i) {
                 max2 = max1;
